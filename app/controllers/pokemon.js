158ea3cde1f843e808b398d57cd723118a41b4c8
moi74 <94870900+moi74@users.noreply.github.com>
2022-11-22 09:57:35 -0300

    Conexão com API

--- app/controllers/pokemon.js
+++ app/controllers/pokemon.js
@@ -1,6 +1,16 @@
-module.exports.buscar = function(application, req, res){
-    let pokemon = req.body;
-    console.log(pokemon.search)
+module.exports.buscar = async function(application, req, res){
+    let pokemon = req.body.search;
 
-    res.render('pokemon/response', {teste : pokemon.search})
+    try{
+        const pokeDados = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
+        .then(response => response.json());
+
+        if(!pokeDados || pokeDados == 'Not Found'){
+            res.send('Pokemon não encontrado!');
+        } else {
+            res.render('pokemon/response', {pokemon : pokemon, pokeDados: pokeDados});
+        }
+    } catch (err) {
+        console.log('Error: ', err.message);
+    }
 }
